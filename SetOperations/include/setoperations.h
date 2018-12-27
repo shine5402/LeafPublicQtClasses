c6ac219dde59d6e671e9ff38205249fd01db3c73
Bo <shine_5402@live.cn>
2018-12-27 14:20:45 +0800

    集合运算：交集和并集

---
+++ SetOperations/include/setoperations.h
@@ -0,0 +1,16 @@
+﻿#ifndef SETOPERATIONS_H
+#define SETOPERATIONS_H
+
+#include "setoperations_global.h"
+
+class SETOPERATIONSSHARED_EXPORT SetOperations
+{
+
+public:
+    template <typename T>
+    static QList<T> getIntersection(QList<QList<T> > lists);
+    template <typename T>
+    static QList<T> getUnion(QList<QList<T> > lists);
+};
+
+#endif // SETOPERATIONS_H
