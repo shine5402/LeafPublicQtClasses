27bc330b6fdcae87a7352ba8c2701e94ea8b0a73
Bo <shine_5402@live.cn>
2018-12-30 10:28:39 +0800

    将集合运算改为仅头文件

--- SetOperations/include/setoperations.h
+++ SetOperations/include/setoperations.h
@@ -1,9 +1,7 @@
 ﻿#ifndef SETOPERATIONS_H
 #define SETOPERATIONS_H
 
-#include "setoperations_global.h"
-
-class SETOPERATIONSSHARED_EXPORT SetOperations
+class SetOperations
 {
 
 public:
@@ -12,5 +10,42 @@ public:
     template <typename T>
     static QList<T> getUnion(QList<QList<T> > lists);
 };
+
+template<typename T>
+QList<T> SetOperations::getIntersection(QList<QList<T> > lists)
+{
+    QHash<T,int> counts;
+    QList<T> result;
+    for (auto list : lists)
+    {
+        for (auto i : list)
+        {
+            counts.insert(i,counts.value(i) + 1);
+        }
+    }
+    for (auto it = counts.begin();it != counts.end();++it)
+    {
+        if (it.value() == lists.count())
+            result.append(it.key());
+    }
+    return result;
+}
+
+template<typename T>
+QList<T> SetOperations::getUnion(QList<QList<T> > lists)
+{
+    QList<T> result;
+    for (auto list : lists)
+    {
+        for (auto i : list)
+        {
+            if (!result.contains(i))
+                result.append(i);
+        }
+    }
+    return result;
+}
+
+
 
 #endif // SETOPERATIONS_H
